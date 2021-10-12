8efdb10b188575eb64e0fcf34717f9c5b6ecad51
P4 <umeaansa555@ed.amdsb.ca>
2021-10-12 13:11:12 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace NumberGuessFlowchartTemplate
+{
+    partial class yesButton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(114, 62);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(63, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Enter Name";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(183, 59);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(178, 20);
+            this.nameInput.TabIndex = 1;
+            this.nameInput.TextChanged += new System.EventHandler(this.nameInput_TextChanged);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(114, 107);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(101, 13);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "What is your name?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Yes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // noButton
+            // 
+            this.noButton.Location = new System.Drawing.Point(268, 176);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(108, 28);
+            this.noButton.TabIndex = 4;
+            this.noButton.Text = "No";
+            this.noButton.UseVisualStyleBackColor = true;
+            // 
+            // yesButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "yesButton";
+            this.Text = "Number Guess";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button noButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace NumberGuessFlowchartTemplate
+{
+    public partial class yesButton : Form
+    {
+        string name;
+
+        public yesButton()
+        {
+            InitializeComponent();
+        }
+
+        private void nameInput_TextChanged(object sender, EventArgs e)
+        {
+            nameInput.Text = name;
+            outputLabel.Text = $"Welcome, {name}!";
+            Thread.Sleep;(1000)
+            Refresh();
+            outputLabel.Text = "Do you want to play a "number guessing game"?";
+
+        }
+    }
+}
